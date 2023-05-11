c0b59066940acd66fc5f297155eb6c347368d720
Sebastian009w <ia3266595@gmail.com>
2023-05-10 22:48:05 -0500

    jest matchers

--- main.test.js
+++ main.test.js
@@ -1,3 +1,9 @@
-test("should return true", () => {
+import { sum } from "./main.js";
+
+it("should return true", () => {
   expect(true);
+});
+
+it("should return 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
