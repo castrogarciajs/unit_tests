6fc8b6ed9de0ee78f00f4a8ac294ee60333480e6
Sebastian009w <ia3266595@gmail.com>
2023-05-10 22:26:57 -0500

    toBeCloseTo

--- matchers/asserts.test.js
+++ matchers/asserts.test.js
@@ -8,3 +8,10 @@ it("should return not", () => {
   expect(3).not.toBe(4);
   expect(3).not.to.toBe(4);
 });
+
+it("should return toBeCloseTo", () => {
+  /**@assert toBeCloseTo */
+  let result = 0.2 + 0.1;
+  expect(result).toBeCloseTo(0.3, 5);
+  expect(result).not.toBeCloseTo(0.3, 50);
+});
