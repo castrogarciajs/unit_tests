7f3fd7397b62fa00e1743098f767533ca606737e
Sebastian009w <ia3266595@gmail.com>
2023-05-10 22:24:00 -0500

    not

--- matchers/asserts.test.js
+++ matchers/asserts.test.js
@@ -1,4 +1,10 @@
-test("should return equal", () => {
+it("should return equal", () => {
   /**@assert toBe */
   expect("Hello world").toBe("Hello world");
+});
+
+it("should return not", () => {
+  /**@assert not to*/
+  expect(3).not.toBe(4);
+  expect(3).not.to.toBe(4);
 });
